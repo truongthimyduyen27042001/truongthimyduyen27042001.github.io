7e86735055c44e030b0c4dface24fadcb8c13af5
truongthimyduyen27042001 <truongthimyduyen27042001@gmail.com>
2021-03-31 00:06:07 +0700

    hello

--- JS/js.js
+++ JS/js.js
@@ -10,13 +10,15 @@ btn.onclick = ()=> {
     let name=document.getElementById("name").value
     for( i=0;i<name.length;i++){
         if(name[i] > "0" && name[i] < "9"){
-           alert("Nhap lai")
+           alert("Full name không có chữ số!.Nhập lại!");
             break 
         } 
         
         let age=document.getElementById("age")
         
     }
+    let age=document.getElementById("age").value
+        if(age<1) alert("Tuổi không thể bé hơn 1.Nhập lại");
     
     
 }
