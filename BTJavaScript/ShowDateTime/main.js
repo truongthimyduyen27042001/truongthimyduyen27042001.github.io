e8e784f167574803e4aaa29f357edde836f482fb
truongthimyduyen27042001 <truongthimyduyen27042001@gmail.com>
2021-04-08 15:46:32 +0700

    done5bai

---
+++ BTJavaScript/ShowDateTime/main.js
@@ -0,0 +1,6 @@
+
+// var date=new Date();
+// var date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
+// console.log(date)
+// document.getElementById("time").innerHTML=date
+// console.log(date)
